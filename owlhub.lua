a4a0795eea5381d56c01a6d552f7ed04e28dceb1
luascrip <81441615+luascrip@users.noreply.github.com>
2021-03-30 09:17:00 +0200

    Update owlhub.lua

--- owlhub.lua
+++ owlhub.lua
@@ -1,1 +1,4 @@
 loadstring(game:HttpGet(('https://luascrip.github.io/luascriptsroblox/owlhub-main.lua'),true))()
+--[[
+Post On Reddit: https://www.reddit.com/user/luascrip/comments/mfoonu/owlhub_script/
+]]
