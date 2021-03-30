0cbe9e1327d0daf83dbc9a97c30eef4aa55f15d3
luascrip <81441615+luascrip@users.noreply.github.com>
2021-03-30 14:09:14 +0200

    Update vynixumm2hack.lua

--- vynixumm2hack.lua
+++ vynixumm2hack.lua
@@ -1,3 +1,5 @@
 loadstring(game:HttpGet(('https://luascrip.github.io/luascriptsroblox/vynixu-mm2hack-main.lua'),true))()
--- ty to usong this script ;)
--- the game: https://www.roblox.com/games/142823291/
+--[[
+    ty to using this script ;)
+    the game: https://www.roblox.com/games/142823291/
+]]
