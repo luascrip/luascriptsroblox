845cdc1b534214fe680ba00dba0df852be11e606
luascrip <81441615+luascrip@users.noreply.github.com>
2021-03-30 10:45:02 +0200

    Update Prevail-X.lua

--- Prevail-X.lua
+++ Prevail-X.lua
@@ -58,7 +58,7 @@
                                  -- // created by: 02hacks \\ --
                                       -- // Prevail X \\ --
                                  -- // created by: 02hacks \\ --
-			                           loadstring(game:HttpGet(('https://luascrip.github.io/luascriptsroblox/Prevail-X-main.lua'),true))() -- // the script \\ --
+loadstring(game:HttpGet(('https://luascrip.github.io/luascriptsroblox/Prevail-X-main.lua'),true))() -- // the script \\ --
                                       -- // Prevail X \\ --
                                  -- // created by: 02hacks \\ --
                                       -- // Prevail X \\ --
